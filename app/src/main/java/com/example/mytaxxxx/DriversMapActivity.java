508d5720548fb4c889a149d9208e35e4919f57d5
Rumpl4Skin <mark.karchmit@yandex.ru>
2021-06-13 13:16:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mytaxxxx/DriversMapActivity.java
@@ -0,0 +1,341 @@
+package com.example.mytaxxxx;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class DriversMapActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        com.google.android.gms.location.LocationListener {
+
+    private GoogleMap mMap;
+    GoogleApiClient googleApiClient;
+    Location lastLocation;
+    LocationRequest locationRequest;
+    Marker PickUpMarker;
+    private MediaPlayer sound;
+
+    private Button LogoutDriverButton, SettingsDriverButton;
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+    private Boolean currentLogoutDriverStatus = false;
+    private DatabaseReference assignedCustomerRef, AssignedCustomerPositionRef;
+    private String driverID, customerID="";
+
+    private ImageView callCustomer;
+    private TextView txtName, txtPhone;
+    private CircleImageView customerPhoto;
+    private RelativeLayout relativeLayout;
+
+    private ValueEventListener AssignedCustomerPositionListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drivers_map);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        driverID = mAuth.getCurrentUser().getUid();
+
+        sound = MediaPlayer.create(DriversMapActivity.this, R.raw.sound);
+        txtName = (TextView)findViewById(R.id.customer_name);
+        txtPhone = (TextView)findViewById(R.id.customer_phone_number);
+        customerPhoto = (CircleImageView)findViewById(R.id.customer_photo);
+        callCustomer = (ImageView) findViewById(R.id.call_to_customer);
+
+        relativeLayout = findViewById(R.id.rel2);
+        relativeLayout.setVisibility(View.GONE);
+        LogoutDriverButton = (Button)findViewById(R.id.driver_logout_button);
+        SettingsDriverButton = (Button)findViewById(R.id.driver_settings_button);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        SettingsDriverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DriversMapActivity.this, SettingsActivity.class);
+                intent.putExtra("type", "Drivers");
+                startActivity(intent);
+            }
+        });
+
+        LogoutDriverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentLogoutDriverStatus = true;
+                mAuth.signOut();
+
+                LogoutDriver();
+                DisconnectDriver();
+            }
+        });
+
+        getAssignedCustomerRequest();
+    }
+
+    private void getAssignedCustomerRequest() {
+        assignedCustomerRef = FirebaseDatabase.getInstance().getReference()
+                .child("Users").child("Drivers").child(driverID).child("CustomerRideID");
+
+        assignedCustomerRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists())
+                {
+                    customerID = dataSnapshot.getValue().toString();
+
+                    getAssignedCustomerPosition();
+                    sound.start();
+
+                    getAssignedCustomerInformation();
+                }
+                else {
+                    customerID = "";
+
+                    if (PickUpMarker!=null)
+                    {
+                        PickUpMarker.remove();
+                    }
+
+                    if(AssignedCustomerPositionListener!= null)
+                    {
+                        AssignedCustomerPositionRef.removeEventListener(AssignedCustomerPositionListener);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void getAssignedCustomerInformation() {
+        relativeLayout.setVisibility(View.VISIBLE);
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference()
+                .child("Users").child("Customers").child(customerID);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists() && dataSnapshot.getChildrenCount()>0)
+                {
+                    String name = dataSnapshot.child("name").getValue().toString();
+                    final String phone  = dataSnapshot.child("phone").getValue().toString();
+
+
+
+                    txtName.setText(name);
+                    txtPhone.setText(phone);
+
+                    callCustomer.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            int permissionCheck = ContextCompat.checkSelfPermission(DriversMapActivity.this, Manifest.permission.CALL_PHONE);
+
+                            if(permissionCheck != PackageManager.PERMISSION_GRANTED){
+                                ActivityCompat.requestPermissions(
+                                        DriversMapActivity.this, new String[]{Manifest.permission.CALL_PHONE}, 123
+                                );
+                            }
+                            else{
+                                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel: " + phone));
+                                startActivity(intent);
+                            }
+                        }
+                    });
+
+                    if (dataSnapshot.hasChild("image")) {
+                        String image = dataSnapshot.child("image").getValue().toString();
+                        Picasso.get().load(image).into(customerPhoto);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void getAssignedCustomerPosition() {
+        AssignedCustomerPositionRef = FirebaseDatabase.getInstance().getReference().child("Customers Requests")
+                .child(customerID).child("l");
+
+        AssignedCustomerPositionListener = AssignedCustomerPositionRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists())
+                {
+                    List<Object> customerPositionMap = (List<Object>) dataSnapshot.getValue();
+                    double locationLat = Double.parseDouble(customerPositionMap.get(0).toString());
+                    double locationLng = Double.parseDouble(customerPositionMap.get(1).toString());
+
+                    LatLng DriverLatLng = new LatLng(locationLat, locationLng);
+                    PickUpMarker = mMap.addMarker(new MarkerOptions().position(DriverLatLng).title("Забрать клиента тут").icon(BitmapDescriptorFactory.fromResource(R.drawable.user)));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(DriverLatLng));
+                    mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        buildGoogleApiClient();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(100000);
+        locationRequest.setFastestInterval(100000);
+        locationRequest.setPriority(locationRequest.PRIORITY_HIGH_ACCURACY);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location)
+    {
+        if(getApplicationContext() != null){
+            lastLocation = location;
+
+            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(12));
+
+            String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+            DatabaseReference DriverAvalablityRef = FirebaseDatabase.getInstance().getReference().child("Driver Available");
+            GeoFire geoFireAvailablity = new GeoFire(DriverAvalablityRef);
+
+            DatabaseReference DriverWorkingRef = FirebaseDatabase.getInstance().getReference().child("Driver Working");
+            GeoFire geoFireWorking = new GeoFire(DriverWorkingRef);
+
+
+            switch (customerID)
+            {
+                case "":
+                    geoFireWorking.removeLocation(userID);
+                    geoFireAvailablity.setLocation(userID, new GeoLocation(location.getLatitude(), location.getLongitude()));
+                    break;
+                default:
+                    geoFireAvailablity.removeLocation(userID);
+                    geoFireWorking.setLocation(userID, new GeoLocation(location.getLatitude(), location.getLongitude()));
+                    break;
+            }
+        }
+
+    }
+
+    protected synchronized void buildGoogleApiClient()
+    {
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        googleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (!currentLogoutDriverStatus) {
+
+            DisconnectDriver();
+        }
+    }
+
+    private void DisconnectDriver()
+    {
+        String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference DriverAvalablityRef = FirebaseDatabase.getInstance().getReference().child("Driver Available");
+
+        GeoFire geoFire = new GeoFire(DriverAvalablityRef);
+        geoFire.removeLocation(userID);
+    }
+
+    private void LogoutDriver()
+    {
+        Intent welcomeIntent = new Intent(DriversMapActivity.this, WelcomeActivity.class);
+        startActivity(welcomeIntent);
+        finish();
+    }
+}
